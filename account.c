1c8d9748591942f6199d21b05e7d9101733c0273
sihyun2988 <sihyun2988@gmail.com>
2022-12-07 17:29:25 +0900

    account added

---
+++ account.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+struct account{
+    char user[80];
+    int accnum;
+    int PIN
+    float balance;
+};
+
+int main()
+{
+    int sdfs;
+}
+
+int openacc()
+{
+    입력: 계좌주 이름, 비밀번호
+    (랜덤, 겹치는 번호있는지 확인)계좌번호 생성
+}
+
+int deposit()
+{
+    sdf
+}
+
+int withdraw()
+{
+    sdf
+}
+
+int 
+
+int pincorrect()
+{
+    계좌주 이름 입력
+    비밀번호를 입력하라는 출력
+    if문으로 판별
+    맞으면 리턴값 0 아니면 리턴값 -1
+}
